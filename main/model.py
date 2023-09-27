aa20d9dc53c536452ef08b1571ee54d24b862ed2
cardbo <william.tweed@hotmail.com>
2023-09-27 08:48:53 +0100

    added methods

--- main/model.py
+++ main/model.py
@@ -1,7 +1,7 @@
 import typing
 
 class Point:
-    def __init__(self,xyz,colour):
+    def __init__(self,xyz,colour=(0,0,0)):
         self.colour = colour
         self.xyz = xyz
         self.x = xyz[0]
@@ -12,8 +12,9 @@ class Point:
         return self.xyz
 
 
+
 class CBModel:
-    def __init__(self,pointmap:typing.List[typing.List[int]]=None,connected_points:typing.List[int]=None):
+    def __init__(self,pointmap:typing.List[Point]=None,connected_points:typing.List[int]=None):
         self.pointmap = pointmap
         self.connected_points = connected_points
 
@@ -24,5 +25,48 @@ class CBModel:
             for i in points:
                 self.pointmap.append(i)
     
-    
-    
+    def delete(self,index:int):  #we need a specific function for this otherwise the indexes will remain in the connected_points arr, leading to errors when rendering connections
+        try:
+            self.pointmap.pop(index)
+            if len(self.connected_points) > 1:
+                for _ in range(0,len(self.connected_points)-1,2):
+                    if self.connected_points[_] == index or self.connected_points[_+1] == index:
+                        self.connected_points.pop(_)
+                        self.connected_points.pop(_+1)
+                
+        except:
+            pass
+
+    @classmethod
+    def load(cls,name):
+        try:
+            with open(f'{name}.CBmodel','r') as model:
+                model_info = model.readlines()
+                model_points = eval((model_info[0]).replace('\n',''))
+                model_connections = eval((model_info[1]).replace('\n',''))
+
+                
+                model.close()
+            connected_points = model_connections
+            points = model_points
+
+            return CBModel(points,connected_points)
+
+        except Exception as e:
+            print(e)
+            print(f'Could not find file {name}.CBmodel; please check your local files.')
+
+        
+
+    def save(self,name):
+        try:
+            with open(f'{name}.CBmodel','w') as writable:
+                writable.write(str(self.pointmap) + '\n')
+                writable.write(str(self.connected_points)+ '\n')
+                writable.close()
+        except:
+            raise WindowsError("Could not save file.")
+        
+
+      
+        
