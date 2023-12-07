83da57c400573375c2e16d7f5ec1f357afdab852
cardbo <william.tweed@hotmail.com>
2023-12-07 14:24:51 +0000

    added version testing

--- main/main.py
+++ main/main.py
@@ -1,6 +1,14 @@
+#run auto-updater
 #force new file creation, or open file
 #give suggestions of previous files
 #give option to start/join classroom service
-
+import auto_updater
 import guizero
 
+def main():
+    version = eval(auto_updater.pull_version())
+    
+
+
+if __name__ == "__main__":
+    main()
