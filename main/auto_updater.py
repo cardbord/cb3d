106050ad5cad3cb9e3553947f3d9c15ed303b037
cardbo <william.tweed@hotmail.com>
2023-12-08 12:27:48 +0000

    removed autoupdate testing

--- main/auto_updater.py
+++ main/auto_updater.py
@@ -8,13 +8,28 @@ def pull_gh(path:str):
         return r.text
     
 def pull_version():
-    return pull_gh("/cardbord/cb3d/main/_globals.py")
+    return pull_gh("/cardbord/cb3d/main/_globals.cblog")
 
 def pull_update():
-    
+
     module_path = Path(__file__).parent
     md_walk = walk(module_path)
     for (dirpath, dirname, filename) in md_walk:
-        print(dirpath,dirname,filename)
+        if not "__pycache__" in dirpath and not ".CBmodel" in filename:
+            path_formatted = dirpath[dirpath.find("main"):].replace("\\","/")
+            
+            for file in filename:
+                if not ".CBmodel" in file and not "auto_updater" in file:
+                    
+                    path = "/cardbord/cb3d/main/"+path_formatted+f"/{file}"
+                    pulled_file = pull_gh(path)
 
-pull_update()
+                    if pulled_file:
+                        openable_path = dirpath.replace("\\","/") + "/"+file
+                        
+                        with open(openable_path,'w') as overwritable:
+                            overwritable.write(pulled_file) 
+                            overwritable.close()
+                    else:
+                        pass # file was not pulled properly due to non-existence/request error, so we pass it,    
+            
