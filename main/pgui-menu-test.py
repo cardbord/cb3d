d7606fe5afe0eeafe5fc9c01fc1be1b23728675f
cardboard <william.tweed@hotmail.com>
2024-02-11 15:27:32 +0000

    Initial commit - main menu testing

---
+++ main/pgui-menu-test.py
@@ -0,0 +1,58 @@
+
+import pygame
+from utils import pgui
+
+
+pygame.init()
+x,y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
+dis = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0])
+
+print(pygame.display.get_desktop_sizes()[0])
+
+menu =  pgui.menu()
+
+
+while 1: #main loop
+     dis.fill((255,255,255))
+     
+     
+     
+     for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+               pygame.quit()
+               quit()
+          elif event.type == pygame.KEYDOWN:
+               if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+               
+                    
+          elif event.type == pygame.MOUSEBUTTONDOWN:
+               if menu.check_windowcollide(x,y):
+                    if menu.File.checkcollide(x,y):
+                         menu.File.is_dropped = not menu.File.is_dropped
+                    if menu.Help.checkcollide(x,y):
+                         menu.Help.is_dropped = not menu.Help.is_dropped
+                         
+                    if menu.check_closebuttoncollide(x,y):
+                         pygame.quit() #completely exit because this is closing the menu
+                         exit()
+           
+                    
+          elif event.type == pygame.MOUSEBUTTONUP:
+               pass
+          
+     menu.display_window(dis)
+
+
+     if menu.check_closebuttoncollide(x,y):
+          menu.clickable_cross.highlighted = True
+     else:
+          menu.clickable_cross.highlighted = False
+
+     
+               
+
+     x,y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
+
+     pygame.display.flip()
