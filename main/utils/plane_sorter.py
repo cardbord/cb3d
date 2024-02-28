cbec345637fcbd7deec89aeba38c65cdf36a97c4
cardbo <william.tweed@hotmail.com>
2024-02-28 18:25:27 +0000

    added quicksort algorithm and more distance calculations

---
+++ main/utils/plane_sorter.py
@@ -0,0 +1,17 @@
+import typing
+#sum([runtime_dis.observer.calc_dist_topoint(runtime_dis.rendered_pointmap[i]) for i in plane])/len(plane[1])
+
+
+def calc_rel_distance(arr,runtime_dis):
+    return sum([runtime_dis.observer.calc_dist_topoint(arr.rpoints[i]) for i in arr.raw_rotates]) / len(arr.raw_rotates) #avg distance
+
+
+def quicksort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+        
+        left = [x for x in array[1:] if x >= pivot]
+        right = [x for x in array[1:] if x < pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
