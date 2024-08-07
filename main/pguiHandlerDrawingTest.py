39153b058da3fe2d39db3737366a1f8f8f02753c
cardboard <william.tweed@hotmail.com>
2024-08-07 18:22:41 +0100

    finished extrude algo

--- main/pguiHandlerDrawingTest.py
+++ main/pguiHandlerDrawingTest.py
@@ -6,43 +6,102 @@ copied from pguiHandlerTest.py, just implementing a Drawing object specifically
 '''
 
 #will add to plane_sorter in utils when done
-def transform(points:list, draw_plane:str,extrusion:int=None):
-    p_array = []
-    add_extrude = False
-    if extrusion != None:
-        add_extrude = True
-    
+
+
+
+def transform(points:list, draw_plane:str,extrusion:str='0',zerodisplacement:str='0'):
+    plane_array = []
+    extrusion=float(extrusion)
+    zerodisplacement=float(zerodisplacement)
     match draw_plane:
         case 'xy':
+            extruded_plane=[]
             for point in points:
-                p_array.append([point[0],point[1],0])
-                if add_extrude:
-                    p_array.append([point[0],point[1],extrusion])
+                extruded_plane.append([point[0],point[1],zerodisplacement])
+            plane_array.append(extruded_plane)
+
+            for i in range(1,len(points)):
+                side_plane = []
+                side_plane.append([[points[i][0],points[i][1],zerodisplacement], [points[i][0],points[i][1],extrusion+zerodisplacement], [points[i-1][0],points[i-1][1],zerodisplacement], [points[i-1][0],points[i-1][1],extrusion+zerodisplacement]])
+                plane_array.append(side_plane)
+            side_plane = []
+            side_plane.append([[points[-1][0],points[-1][1],zerodisplacement], [points[-1][0],points[-1][1],extrusion+zerodisplacement], [points[0][0],points[0][1],zerodisplacement], [points[0][0],points[0][1],extrusion+zerodisplacement]])
+            plane_array.append(side_plane)
+
         case 'xz':
+            extruded_plane = []
             for point in points:
-                p_array.append([point[0], 0, point[1]])
-                if add_extrude:
-                    p_array.append([point[0],extrusion,point[1]])
-        case 'yz':
-            for point in points:
-                p_array.append([0, point[0], point[1]])
-                if add_extrude:
-                    p_array.append([extrusion,point[0],point[1]])
-                
-    return p_array 
+                extruded_plane.append([point[0],zerodisplacement,point[1]])
+            plane_array.append(extruded_plane)
+
+
+            for i in range(1,len(points)):
+                side_plane = []
+                side_plane.append([[points[i][0],zerodisplacement,points[i][1]], [points[i][0],extrusion+zerodisplacement,points[i][1]], [points[i-1][0],zerodisplacement,points[i-1][1]], [points[i-1][0],extrusion+zerodisplacement,points[i-1][1]]])
+                plane_array.append(side_plane)
+            side_plane = []
+            side_plane.append([[points[-1][0],zerodisplacement,points[-1][1]], [points[-1][0],extrusion+zerodisplacement,points[-1][1]], [points[0][0],zerodisplacement,points[0][1]], [points[0][0],extrusion+zerodisplacement,points[0][1]]])
+            plane_array.append(side_plane)
         
+        case 'yz':
+            extruded_plane = []
+            for point in points:
+                extruded_plane.append([zerodisplacement,point[0],point[1]])
+            plane_array.append(extruded_plane)
+
+
+            for i in range(1,len(points)):
+                side_plane = []
+                side_plane.append([[zerodisplacement,points[i][0],points[i][1]], [extrusion+zerodisplacement,points[i][0],points[i][1]], [zerodisplacement,points[i-1][0],points[i-1][1]], [extrusion+zerodisplacement,points[i-1][0],points[i-1][1]]])
+                plane_array.append(side_plane)
+            side_plane = []
+            side_plane.append([[zerodisplacement,points[-1][0],points[-1][1]], [extrusion+zerodisplacement,points[-1][0],points[-1][1]], [zerodisplacement,points[0][0],points[0][1]], [extrusion+zerodisplacement,points[0][0],points[0][1]]])
+            plane_array.append(side_plane)
+            
+    
+
+        
+def buildTransform(points,planetype):
+    input_options = h3.collate_textinput_inputs()
+    print(input_options)
+    print(transform(points,planetype,input_options['Extrusion'],input_options['From']))
 
 def demoChildXYZselector(points): #this is implemented inside run_3d.py
-    d = pgui.GUIobj([0,0],[200,600],'test')
+    d = pgui.GUIobj([0,0],[452,1000],'demochildXYZselector')
     d.points = points
+
+    
+
     d.add_content(
+        
         pgui.DisplayRows([
-            pgui.Image([0,0],'boxo.png').set_callback(lambda: print(transform(d.points,'xy'))), #these will be custom 200x200 images i'll make later. for now... boxo.png!
-            pgui.Image([0,0],'boxo.png').set_callback(lambda: print(transform(d.points,'xz'))),
-            pgui.Image([0,0],'boxo.png').set_callback(lambda: print(transform(d.points,'yz')))
+            pgui.Image([0,0],'boxo_xy.png').set_callback(lambda: buildTransform(d.points,'xy')),
+            pgui.Image([0,0],'boxo_xz.png').set_callback(lambda:buildTransform(d.points,'xz')),
+            pgui.Image([0,0],'boxo_yz.png').set_callback(lambda:buildTransform(d.points,'yz')),
+            pgui.DisplayRows([
+                None,
+                pgui.DisplayColumns([
+                    pgui.Text([0,0],'From',pgui.TextType.h3),None
+                ]), 
+                
+                pgui.TextInput([0,0],'','0',1,'From'),
+                None
+            ]),
             
-            #we use the anonymous function lambda: cbmod.add_plane(transform(d.points,planetype), [], texture) in the real thing
-        ])
+            pgui.DisplayRows([
+                None,
+                pgui.DisplayColumns([
+                    pgui.Text([0,0],'Extrusion',pgui.TextType.h3),None
+                ]), 
+                pgui.TextInput([0,0],'','0',1,'Extrusion'),
+                None
+            ])
+            
+            
+            #we use the anonymous function lambda: cbmod.add_plane(buildTransform(d.points,planetype), [], texture) in the real thing
+        ]),
+            
+        
     )
     return d
 
@@ -74,6 +133,8 @@ while 1:
             
             #within this loop, it is beneficial to provide the event, and then feed it through pgui's handler. it's far better than running two event loops at once
     
+    for event in h3.eventLog:
+        print(event)
     
     h3.display(dis)
     pygame.display.flip()
