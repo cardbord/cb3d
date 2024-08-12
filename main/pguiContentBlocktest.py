ff289027c319bb2ba2a302157884af01e8c1985a
cardboard <william.tweed@hotmail.com>
2024-08-12 13:46:19 +0100

    removed debugcode and handlercode (for now)

--- main/pguiContentBlocktest.py
+++ main/pguiContentBlocktest.py
@@ -1,9 +1,5 @@
-from utils.pgui import TextInput, Button, DisplayColumns, DisplayRows, GUIobj, calc_rel_size, Handler, Anchor
+from utils.pgui import TextInput, Button, DisplayColumns, DisplayRows, GUIobj, calc_rel_size
 import pygame
-
-eventHandler = Handler()
-
-debug = False #SET FALSE WHEN NOT TESTING
 
 pygame.init()
 
@@ -28,7 +24,7 @@ dis = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0])
 size_Sf = calc_rel_size()
 
 
-eventHandler.add(obj)
+
 
 print("STARTING MAIN LOOP")
 xy = pygame.mouse.get_pos()
@@ -38,7 +34,6 @@ while 1: #main loop
      
      for event in pygame.event.get():
           
-          eventHandler.handle_event(event,x,y)
           if event.type == pygame.QUIT:
                pygame.quit()
                quit()
@@ -47,13 +42,7 @@ while 1: #main loop
                     pygame.quit()
                     quit()
 
-     eventHandler.display(dis)
-     print(eventHandler.collate_textinput_inputs())
-     if debug: #would it be worth actually making this a feature at some point? if i keep having these ideas i'm never gonna be finished...
-          displace_height = 50*obj._SIZE_SF
-          avg_h = (obj.content[0].parent_window_size[1] - displace_height)/len(obj.content[0].content)
-          for i in range(len(obj.content[0].content)):
-               pygame.draw.line(dis,(0,0,0),(obj.pos[0],obj.pos[1] +displace_height + avg_h*i),(obj.pos[0]+obj.window_size[0], obj.pos[1]+displace_height+avg_h*i))
+     obj.display_window(dis)
           
      x,y = pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]
      pygame.display.update()
