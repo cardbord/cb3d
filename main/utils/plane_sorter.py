38c196720ad69313db820503e59904d55e685c33
cardboard <william.tweed@hotmail.com>
2024-08-12 13:51:52 +0100

    uhm... why is my xyz zxy?

--- main/utils/plane_sorter.py
+++ main/utils/plane_sorter.py
@@ -18,7 +18,7 @@ def transform(points:list, draw_plane:str,extrusion:str='0',zerodisplacement:str
     extrusion=float(extrusion)
     zerodisplacement=float(zerodisplacement)
     match draw_plane:
-        case 'xz':
+        case 'xy':
             original_plane=[]
             extruded_plane=[]
             for point in points:
@@ -36,7 +36,7 @@ def transform(points:list, draw_plane:str,extrusion:str='0',zerodisplacement:str
             side_plane.extend([[points[-1][0],points[-1][1],zerodisplacement], [points[-1][0],points[-1][1],extrusion+zerodisplacement], [points[0][0],points[0][1],extrusion+zerodisplacement], [points[0][0],points[0][1],zerodisplacement]])
             plane_array.append(side_plane)
 
-        case 'xy':
+        case 'xz':
             original_plane=[]
             extruded_plane = []
             for point in points:
@@ -58,18 +58,18 @@ def transform(points:list, draw_plane:str,extrusion:str='0',zerodisplacement:str
             original_plane=[]
             extruded_plane = []
             for point in points:
-                original_plane.append([zerodisplacement,point[0],point[1]])
-                extruded_plane.append([extrusion+zerodisplacement,point[0],point[1]])
+                original_plane.append([zerodisplacement,point[1],point[0]])
+                extruded_plane.append([extrusion+zerodisplacement,point[1],point[0]])
             plane_array.append(original_plane)
             plane_array.append(extruded_plane)
 
 
             for i in range(1,len(points)):
                 side_plane = []
-                side_plane.extend([[zerodisplacement,points[i][0],points[i][1]], [extrusion+zerodisplacement,points[i][0],points[i][1]], [extrusion+zerodisplacement,points[i-1][0],points[i-1][1]], [zerodisplacement,points[i-1][0],points[i-1][1]]])
+                side_plane.extend([[zerodisplacement,points[i][1],points[i][0]], [extrusion+zerodisplacement,points[i][1],points[i][0]], [extrusion+zerodisplacement,points[i-1][1],points[i-1][0]], [zerodisplacement,points[i-1][1],points[i-1][0]]])
                 plane_array.append(side_plane)
             side_plane = []
-            side_plane.extend([[zerodisplacement,points[-1][0],points[-1][1]], [extrusion+zerodisplacement,points[-1][0],points[-1][1]], [extrusion+zerodisplacement,points[0][0],points[0][1]], [zerodisplacement,points[0][0],points[0][1]]])
+            side_plane.extend([[zerodisplacement,points[-1][1],points[-1][0]], [extrusion+zerodisplacement,points[-1][1],points[-1][0]], [extrusion+zerodisplacement,points[0][1],points[0][0]], [zerodisplacement,points[0][1],points[0][0]]])
             plane_array.append(side_plane)
 
     
